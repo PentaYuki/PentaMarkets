d8b5485b11b750686a9c04377f8a0dea54ade161
PentaYuki <gooleseswsq1@gmail.com>
2026-05-16 15:51:54 +0900

    Upgrade SW to v3 with cache cleanup and update footer version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pentamarket-v2';
+const CACHE_NAME = 'pentamarket-v3';
 const ASSETS = [
   '/',
   '/taphoapos.html',
@@ -12,6 +12,18 @@ const ASSETS = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Activate Event - Cleanup old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
   );
 });
 
